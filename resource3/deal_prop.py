8f258d0789a2a19391d2f6e27f14069c48cc1f7a
wcy666good <83395924+wcy666good@users.noreply.github.com>
2022-12-04 16:20:21 +0800

    Add files via upload
    the forth submit: GA optimization

---
+++ resource3/deal_prop.py
@@ -0,0 +1,18 @@
+import numpy as np
+import sys
+import os
+
+from BEMT_program.solver import Solver
+
+class HiddenPrints:
+    def __enter__(self):
+        self._original_stdout = sys.stdout
+        sys.stdout = open(os.devnull, 'w')
+ 
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        sys.stdout.close()
+        sys.stdout = self._original_stdout
+
+if __name__ == "__main__":
+
+    1
